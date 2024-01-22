0f64831a6e9d46b3f5ba89cee719178afb570010
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2024-01-22 16:05:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/frost/portfolio/video_conference_skytalk/MainActivity.kt
@@ -0,0 +1,6 @@
+package frost.portfolio.video_conference_skytalk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+double? screenWidth;
+double? screenHeight;
+
+// Color Ver 1
+// https://coolors.co/palette/006d77-83c5be-edf6f9-ffddd2-e29578
+
+Color kPrimary = const Color(0xff83C5BE);
+Color kSecondary = const Color(0xffE29578);
+Color kTertiary = const Color(0xffFFDDD2);
+Color kWhite = const Color(0xffEDF6F9);
+Color kBlack = const Color(0xff006D77);
